f92470b851238c89b1fb73172575bb68c6c73f9f
JoelDeoKL <joeldeocm88@gmail.com>
2021-05-10 15:39:46 +0200

    Debut de la partie front-end

---
+++ ressources/inc/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Action vérité</title>
+
+    <link rel="icon" type="image/png" href="public/images/logo.png">
+	<link rel="stylesheet" href="public/css/bootstrap.css">	
+	<link rel="stylesheet" href="public/style.css">
+    <link rel="stylesheet" href="public/css/responsive.css">
+    <link rel="stylesheet" href="public/css/animate.css">
+</head>
+<body>
+    
+</body>
+</html>
