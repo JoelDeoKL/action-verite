5836e56864de7ce8c91a054d8d9ba33a34cb900e
JoelDeoKL <joeldeocm88@gmail.com>
2021-06-13 00:38:26 +0200

    Ajout d'un controller

---
+++ views/choix.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 col-md-6 col-12">
+            <div style="text-align:center">
+                <h1>Faites votre choix</h1>
+                <br>
+                <a href="" class="btn btn-primary">Action</a>
+                <a href="" class="btn btn-primary">Vérité</a>
+            </div>
+        </div>
+    </div>
+</div>
