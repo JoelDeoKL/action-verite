436ad3018ba7d1b06461996852011f8968d6acde
JoelDeoKL <joeldeocm88@gmail.com>
2021-06-16 05:06:47 +0200

    Mise en place d'une fonction de verification de compte

--- models/UsersModel.php
+++ models/UsersModel.php
@@ -1,7 +1,20 @@
 <?php
 
+require_once("entites/Users.php");
+require_once("x_models/MainModel.php");
+
 class UsersModel extends MainModel{
 
-    //
+    public function check(Users $users){
+        $query = "SELECT * FROM users WHERE email=?";
+        $sql = self::pdo()->prepare($query);
+
+        $sql->execute([$users->getEmail()]);
+
+
+    }
+    public function inscription(){
+
+    }
 
 }
