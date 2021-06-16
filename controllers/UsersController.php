0744fd869009b964caa13ebb305a4d3d2e288f3c
JoelDeoKL <joeldeocm88@gmail.com>
2021-06-16 04:42:16 +0200

    Mise en place su systeme d'inscription dans le controller Users

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -8,6 +8,20 @@ class UsersController extends x_Controller{
     }
 
     public function inscription(){
+        if(isset($_POST["inscrire"])){
+            $this->inscrire();
+        }
         $this->load->view("inscription");
+    }
+    public function inscrire(){
+        $pseudo = $_POST["pseudo"];
+        $email = $_POST["email"];
+        $mdp = password_hash($_POST["mdp"], PASSWORD_BCRYPT);
+
+        if($_POST["mdp"] == $_POST["confirm"]){
+            var_dump($pseudo. " " . $email . " " . $mdp);
+            die();
+        }
+
     }
 }
