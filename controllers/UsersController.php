1e8168422b58bf337ed4e465da972daf68924a05
JoelDeoKL <joeldeocm88@gmail.com>
2021-06-16 04:18:03 +0200

    mis en place du systeme avec Base de donnees

---
+++ controllers/UsersController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class UsersController extends x_Controller{
+
+    public function compte(){
+        echo "OH";
+    }
+
+    public function inscription(){
+        var_dump("OK");
+    }
+}
