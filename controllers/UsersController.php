1031041d8645337d1cfeb1f89a548a7a3a4de171
JoelDeoKL <joeldeocm88@gmail.com>
2021-07-07 11:46:29 +0200

    Mise en place des methodes dans le controller pour connexion

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -34,4 +34,39 @@ class UsersController extends x_Controller{
             }
         }
     }
+
+    public function connect(){
+        if(isset($_POST['connexion'])){
+            $this->connexion();
+        }
+        $this->load->view("connexion");
+    }
+
+    public function connexion(){
+        $this->load->model("UsersModel");
+        $email = $_POST["email"];
+        $psw = $_POST["mdp"];
+
+        $mdp = password_hash($psw, PASSWORD_BCRYPT);
+
+        $users = new Users(null, null, $email, $mdp);
+        $connexion = new UsersModel();
+
+        if($connexion->check($users)){
+            if($connexion->connexion($users)){
+                header('location: index.php?kay=x-users.compte');
+            }
+        }else{
+            header('Location: index.php?kay=x-users.connect');
+        }
+    }
+
+    public function deconnexion(){
+        if(isset($_SESSION['nom'])){
+            session_destroy();
+            header('Location: index.php');
+            exit();
+            return $html;
+        }
+    }
 }
